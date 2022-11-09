a98e35c8917ee6586da88716836a05cbdce2b2e1
mishashostak <114440497+mishashostak@users.noreply.github.com>
2022-11-08 21:38:08 -0500

    progress :/

--- AgentTest.java
+++ AgentTest.java
@@ -1,10 +1,24 @@
+import java.util.*;
+
 public class AgentTest {
     
-    // Testing of the Chat Bot
+    // Testing of the Virtual Agent
     public static void main(String[] args) {
-        
-        JShizzle J = new JShizzle(null);
-        System.out.println("/n"+JShizzle.right());
+        JShizzle J = new JShizzle("J Shizzle");
+        try (Scanner input = new Scanner(System.in)) {
+            while(true) {
+                System.out.print(J.getUserName() + ": ");
+                String inp = input.nextLine();
+                String response = J.respondToUserQuery(inp);
+                if(response.equals("bye")) {
+                    break;
+                } else if (response.equals("")) {
+                    continue;
+                } else {
+                    System.out.print(J.getName() + ": ");
+                    System.out.println(response);
+                }
+            }
+        }
     }
-
 }
